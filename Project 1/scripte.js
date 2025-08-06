77527f9c20a4f6b796717a8e111d988b90aec258
Hairo <hasnaouinidal308@gmail.com>
2025-08-06 19:24:45 +0100

    finish the project !

--- Project 1/scripte.js
+++ Project 1/scripte.js
@@ -8,6 +8,17 @@ function addTask(){
         let new_item = document.createElement('li'); 
         new_item.textContent = task.value ; 
         container.appendChild(new_item);
+        let span = document.createElement('span'); 
+        span.innerHTML = '\u00d7'; 
+        new_item.appendChild(span);
     }
     task.value = '';
+
+    container.addEventListener('click', function(e){
+        if(e.target.tagName === 'LI'){
+            e.target.classList.toggle('checked'); 
+        }else if (e.target.tagName === 'SPAN'){
+            e.target.parentElement.remove(); 
+        }
+    })    
 }

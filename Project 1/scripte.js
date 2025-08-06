354a5ac7dd5aa260642365a2f51c1d2c775d5b10
Hairo <hasnaouinidal308@gmail.com>
2025-08-06 13:27:02 +0100

    this is how you can create a simple function can do this bro !

--- Project 1/scripte.js
+++ Project 1/scripte.js
@@ -2,5 +2,16 @@
 
 function addTask(){
     let task = document.getElementById('input-box').value ;
-    console.log(task)
+    let container = document.getElementById('list-container'); 
+
+    if(task.trim() === ''){
+        alert('plz enter a task !')
+        return
+    }else{
+        let newItem = document.createElement('li'); 
+        newItem.textContent = task  ; 
+
+        container.appendChild(newItem);
+    }
+
 }

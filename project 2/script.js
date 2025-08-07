94752546c1b9926d6b6477f15c4f40be75d9c2ca
Hairo <hasnaouinidal308@gmail.com>
2025-08-07 02:29:49 +0100

    finish the fuking project 2 !

--- project 2/script.js
+++ project 2/script.js
@@ -1,1 +1,19 @@
+let container = document.getElementById('main'); 
 
+function Add_note(){
+    let not = document.createElement('div'); 
+    not.classList.add('note'); 
+    not.innerHTML = `
+        <div class="tool">
+                <i class="fas fa-save"></i> 
+                <i class="fas fa-trash trash"></i> 
+            </div>
+            <textarea></textarea>
+    `; 
+    container.appendChild(not); 
+    let delet = not.querySelector('.trash'); 
+    delet.addEventListener('click', function(){
+        not.remove();
+    })
+
+}

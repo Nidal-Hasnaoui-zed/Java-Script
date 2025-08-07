c297eee603e9be7cb948417221a6dcb712d7f91a
Hairo <hasnaouinidal308@gmail.com>
2025-08-07 12:37:49 +0100

    thsi id me bro ! and i finish password1

--- project 3/script.js
+++ project 3/script.js
@@ -1,17 +1,30 @@
 let password = document.getElementById('password');
-
+let strength = document.getElementById('strength'); 
+let message  = document.getElementById('message');
+let btn = document.getElementById('button'); 
 password.addEventListener('input', function(){
     let password_value = password.value ; 
     let password_len = password_value.length ;
 
-    password_msg = ''
-    if(password_len < 5){
-        password_msg = ''
-    } else if(password_len >= 5){
-        password_msg = 'weak'; 
-    } else if(password_len >= 10){
-        password_msg = 'mid'; 
+    pwd_msg = ''
+    if(password_len === 0){
+        pwd_msg = ''
+    } else if(password_len < 5){
+        pwd_msg = 'weak'; 
+    } else if(password_len < 10){
+        pwd_msg = 'mid'; 
     }else{
-        password_msg = 'strong';
+        pwd_msg = 'strong';
+    }
+    strength.textContent = pwd_msg ; 
+    message.style.display = 'block';
+})
+
+btn.addEventListener('click', function(){
+    let pwd_type = password.getAttribute('type'); 
+    if(pwd_type === 'password'){
+        password.setAttribute('type','text'); 
+    }else{
+        password.setAttribute('type','password'); 
     }
 })

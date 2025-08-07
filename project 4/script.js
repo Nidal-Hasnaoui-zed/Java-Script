4f9cc9c5000d1584ac712a6fc312c71a3e6c1a78
Hairo <hasnaouinidal308@gmail.com>
2025-08-07 12:53:07 +0100

    this me bro how are u bro !

--- project 4/script.js
+++ project 4/script.js
@@ -8,3 +8,14 @@ const numberEl = document.getElementById("numbers");
 const symbolsEl = document.getElementById("symbols");
 const generateBtn = document.getElementById("getBtn");
 const copyIcon = document.getElementById("copyIcon");
+
+// lets add what we need !
+let lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"; 
+let uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ; 
+let numbers = "0123456789";
+let symbols = "!@#$%^&*()_+-=[]{}\\|;':\",./<>?";
+
+sliderValue.textContent = inputSlider.value ; 
+inputSlider.addEventListener('input', function(){
+    sliderValue.textContent = inputSlider; 
+})

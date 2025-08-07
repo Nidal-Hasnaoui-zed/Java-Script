ef242f9097da7df0de0efb5c46e9e779d8620f5d
Hairo <hasnaouinidal308@gmail.com>
2025-08-07 12:46:14 +0100

    getting element form the html doc !

---
+++ project 4/script.js
@@ -0,0 +1,10 @@
+// getign elemnts from html doc !
+const inputSlider = document.getElementById("inputSlider");
+const sliderValue = document.getElementById("sliderValue");
+const passBox = document.getElementById("passBox");
+const lowercaseEl = document.getElementById("lowercase");
+const uppercaseEl = document.getElementById("uppercase");
+const numberEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateBtn = document.getElementById("getBtn");
+const copyIcon = document.getElementById("copyIcon");
